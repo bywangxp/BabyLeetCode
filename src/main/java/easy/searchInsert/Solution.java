60215bc93a19f7e2c3b3e055ff5f08c0d96bec31
alsoran <546691648@qq.com>
2023-07-11 01:36:30 +0800

    二分法

--- src/main/java/easy/searchInsert/Solution.java
+++ src/main/java/easy/searchInsert/Solution.java
@@ -1,5 +1,9 @@
 package easy.searchInsert;
 
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 /**
  * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
  * <p>
@@ -7,36 +11,105 @@ package easy.searchInsert;
  */
 public class Solution {
     /**
-     * 二分查找
-     *
+     * 闭区间
      * @param nums
      * @param target
      * @return
      */
-    public int searchInsert(int[] nums, int target) {
-        if (nums.length == 0) {
+    public static int searchInsert(int[] nums, int target) {
 
-        }
-        if (nums.length == 1) {
-            if (nums[0] == target) {
-                return 1;
+        int first = 0;
+        int last = nums.length - 1;
+
+        while (first <= last ) {
+
+            int mid = first + (last - first) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[mid] < target) {
+                first = mid + 1;
+            } else {
+                last = mid - 1;
             }
         }
-        int mid = nums.length / 2;
-        if (nums[mid] == target) {
-            // 找到位置了
-            return mid;
-        }
-        if (nums[mid] < target) {
-
-        }
+        return first;
 
 
-        return 0;
+
     }
 
-    public int searchInsert(int[] num, int first, int last, int target) {
-        return 0;
+    /**
+     * 半开半闭区
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int searchInsert2(int[] nums, int target) {
+
+        int first = 0;
+        int last = nums.length;
+
+        while (first < last ) {
+
+            int mid = first + (last - first) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[mid] < target) {
+                first = mid + 1;
+            } else {
+                last = mid;
+            }
+        }
+        return first;
+
+
+    }
+
+    public static int searchInsert3(int[] nums, int target) {
+
+        int first = -1;
+        int last = nums.length;
+
+        while (first+1 < last ) {
+
+            int mid = first + (last - first) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[mid] < target) {
+                first = mid ;
+            } else {
+                last = mid;
+            }
+        }
+        return first +1;
+
+
+    }
+
+    public static void main(String[] args) {
+        assertEquals(searchInsert(new int[]{1, 3, 5, 6}, 5),2);
+        assertEquals(searchInsert(new int[]{1, 3, 5, 6}, 2),1);
+        assertEquals(searchInsert(new int[]{1, 3, 5, 6}, 7),4);
+        assertEquals(searchInsert(new int[]{1, 3, 5, 6}, 0),0);
+        System.out.println("over1");
+
+
+        assertEquals(searchInsert2(new int[]{1, 3, 5, 6}, 5),2);
+        assertEquals(searchInsert2(new int[]{1, 3, 5, 6}, 2),1);
+        assertEquals(searchInsert2(new int[]{1, 3, 5, 6}, 7),4);
+        assertEquals(searchInsert2(new int[]{1, 3, 5, 6}, 0),0);
+        System.out.println("over2");
+
+        // 0 , 3   0+3/2 = 1   1+3/2 = 2 33
+        assertEquals(searchInsert3(new int[]{1, 3, 5, 6}, 5),2);
+        assertEquals(searchInsert3(new int[]{1, 3, 5, 6}, 2),1);
+        assertEquals(searchInsert3(new int[]{1, 3, 5, 6}, 7),4);
+        assertEquals(searchInsert3(new int[]{1, 3, 5, 6}, 0),0);
+        System.out.println("over3");
+
     }
 
 
